fb70ab125d83d6b9f951d02a44c81d073a7d01ea
Chris Rabuse <rab233@live.com>
2016-04-07 11:45:24 -0500

    Refactored to use AngularJS

--- app.js
+++ app.js
@@ -1,73 +1,36 @@
-var search = "";
+var app = angular.module('gifD', []);
 
-$(function(){
-  $('button').on("click", function(){
-    // Gets input and replaces spaces with +
-    search = $('input').val().replace(/ /g,"+");
+app.controller('GifController', function($scope, $http) {
+  $scope.search = "";
 
-    // console.log(search);
-    gifIt(search);
-  });
+  $scope.getGif = function(search) {
+    console.log("getGif function ran")
+    $scope.searchData = [];
+    $scope.showRandom = false;
+    // Chek to see if search is empty
+    if (search != "") {
+      // Set the api search
+      var searchApi = 'http://api.giphy.com/v1/gifs/search?q=' + search +'&api_key=dc6zaTOxFJmzC';
 
-  $('input').keypress(function(e) {
-    // Gets what key was pressed
-    var code = e.keyCode || e.which;
+      // Get information
+      $http.get(searchApi).then(function(response) {
+        // console.log(response);
 
-    // Checks to see if the enter key was pressed
-    if (code == 13) {
-      // Get input from text box
-      search = $('input').val().replace(/ /g,"+");
+        // Set array to
+        $scope.searchData = response.data.data;
 
-      // Change placeholder
-      var placeholder = $("input").val();
-      if(placeholder != "") {
-        $('input').attr("placeholder", placeholder);
-      }
+        // Hides the random image
+        $scope.showRandom = false;
+      });
+    } else {
+      // Get random image
+      $http.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC').then(function(response){
+        $scope.searchData = response.data.data;
+        console÷.log(response);
 
-      // Clear textbox
-      $('input').val("");
-
-      // Get the gifs
-      gifIt(search);
+        // Show the random image
+        $scope.showRandom = true;
+      });
     }
-  });
+  };
 });
-
-function gifIt(search) {
-
-  // Chek to see if search is empty
-  if (search != "") {
-    // Remove any gifs on page
-    $(".gifs").empty();
-
-    // Set the api search
-    var searchApi = 'http://api.giphy.com/v1/gifs/search?q=' + search +'&api_key=dc6zaTOxFJmzC';
-
-    // Get information
-    $.get(searchApi).done(function(response) {
-      // console.log(response);
-
-      // Loop through results and show images
-      for (var i = 0; i < response.data.length; i++) {
-        $('.gifs').append("<img src=\"" + response.data[i].images.fixed_height.url + "\" />");
-      }
-    });
-  } else {
-
-    // Clears gifs if attribute is not random
-    if($('input').attr("placeholder") != "Random") {
-      $('.gifs').empty();
-    }
-    // Change placholder to Random
-    $('input').attr("placeholder", "Random");
-
-    // Get random image
-    $.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC').done(function(response){
-      // Add image to DOM
-      $('.gifs').append("<img src=\"" + response.data.fixed_height_downsampled_url + "\" />");
-      // console.log(response);
-    }).fail(function(response){
-      console.log("No gif's :(");
-    });
-  }
-}
